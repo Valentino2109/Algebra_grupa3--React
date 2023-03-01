1fe0379cc3dbb503b6421e11047a6b083b8130bf
Valentino Benković <vbenkovic92@gmail.com>
2023-03-01 20:54:48 +0100

    on effects

--- React/React/Predavanja/pred3/test-app/src/App.js
+++ React/React/Predavanja/pred3/test-app/src/App.js
@@ -4,10 +4,13 @@ import ArrowFunc from "./Components/ArrowFunc";
 import MyComponent from "./state/MyComponent";
 import ChildrenComponent from "./state/ChildrenComponent";
 import FuncComponent from "./state/FuncComponent";
+import CountRootComponent from "./Event/CountRootComponent";
 
 function App() {
   return (
     <div>
+      <CountRootComponent />
+      <hr />
       <ShowText ime="Pero" pozdrav={`Dobro jutro`} />
       <ShowMeTheMoney money={67} />
       <ArrowFunc />
